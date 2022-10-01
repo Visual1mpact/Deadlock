610ea67cf819f989c3562178ebf0e016ec75c505
Visual1mpact <97915078+Visual1mpact@users.noreply.github.com>
2022-10-01 11:26:39 -0400

    Reverse loop in ecwipe command

--- scripts/command/Utility/ecwipe.ts
+++ scripts/command/Utility/ecwipe.ts
@@ -28,7 +28,7 @@ export function ecwipe(message: BeforeChatEvent, args: string[]) {
     }
 
     // There are 30 slots ranging from 0 to 29
-    for (let slot = 0; slot < 30; slot++) {
+    for (let slot = 29; slot >= 0; slot--) {
         /**
          * Try/Catch guard is needed since the container for the enderchest is not exposed for the API.
          * We have to use a function command to clear the chest and since we can't properly determine if slots
